bd9c44c17bea6dbbf51b70f2cb57c106b861ff04
Luisa Vaca <lvaca@hugeinc.com>
2016-02-29 13:13:15 -0500

    limit

--- reduce.js
+++ reduce.js
@@ -4,19 +4,10 @@ var arr = [2,3,8,1];
   var e = arr.reduce(function(arrLarge, nums){
     for(var i = 0; i < nums; i++){
      var a = nums;
-     a = Math.floor((Math.random() * 10));
+     a = Math.floor((Math.random() * nums));
      arrLarge.push(a);
    }
    return arrLarge;
   },[])
   console.log(e);
 })();
-
-var str = "Hola";
-
-function doubleChart(str){
-  var newStr = " ";
-  for(var i = 0; i < str.length; i ++){
-    newStr = str[i]+ str[i];
-  }
-}
